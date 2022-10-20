ec2619e1f03baeadb45f464e654a68701362528c
thejoeseifert <78624450+thejoeseifert@users.noreply.github.com>
2022-10-20 11:24:25 -0400

    Add files via upload

--- Dancing Robot Project/src/main.cpp
+++ Dancing Robot Project/src/main.cpp
@@ -24,26 +24,28 @@ void loop() {
   Encoder enc1(M1_ENC_A, M1_ENC_B);
   Encoder enc2(M2_ENC_A, M2_ENC_B);
   int start = 0;
-  int move_req = 0;
-  int last_mode = 0;
+  brain1.move_req = 1;
+  int last_mode = 1;
 
   //stays here after initial
   while(true) {
   //rx message from server
   rx_udp(&brain1);
 
+
   //default circle is 0
-  if (brain.move_req != last_mode)  {
+  if (brain1.move_req != last_mode)  {
     start = 0;
   }
+
   float* speedangle;//2 variable pointer[0] is v, [1] is w
-  speedangle = defaultLoop(enc1, enc2, start, brain1.move_req);  //TrajectoryTracking loop, should later be modified to change loop shape
+  speedangle = defaultLoop(enc1, enc2, start, brain1.move_req);  
   Serial.print("\tvelocity: "); Serial.print(speedangle[0]); Serial.print("\t"); Serial.print("angle: ");Serial.print(speedangle[1]); 
   Serial.print("\n");
   start = 1;
   mouse1.heading_meas = speedangle[1];
   mouse1.velocity_meas = speedangle[0];
-  last_mode = move_req;
+  last_mode = brain1.move_req;
   
   //send to server
   send_udp(&mouse1);
