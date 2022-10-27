aff1f6a6d820a29732c70530aa84006c9a66da74
thejoeseifert <78624450+thejoeseifert@users.noreply.github.com>
2022-10-27 06:30:39 -0700

    Add files via upload

--- jetson side/udp_server.py
+++ jetson side/udp_server.py
@@ -5,7 +5,7 @@ import struct
 
 HEADERSIZE = 10 
 
-localIP     = "192.168.26.101"
+localIP     = "192.168.105.101"
 
 localPort   = 3333
 
@@ -14,8 +14,8 @@ bufferSize  = 1024
  
 
 msgFromServer       = "Hello UDP Client"
-heading_req = 14.56
-v_req = 13.43
+heading_req = 0
+v_req = 0
 move_req = 1
 
 coord_struct = struct.pack('ffi', heading_req, v_req, move_req)
@@ -31,6 +31,7 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPServerSocket.bind((localIP, localPort))
 
+#need to create two threads here
  
 
 print("UDP server up and listening")
@@ -42,19 +43,58 @@ print("UDP server up and listening")
 while(True):
 
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-    message = bytesAddressPair[0]
-
-    address = bytesAddressPair[1]
-
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
+    mouse_1 = [0,0]
+    mouse_2 = [0,0]
+    mouse_3 = [0,0]
     
-    print(clientMsg)
-    print(clientIP)
-    print(struct.unpack('ffffiif', message))
-   # Sending a reply to client
+    if mouse_1[1] == 0:
+        mouse_1[0] = bytesAddressPair[0]
+        mouse_1[1] = bytesAddressPair[1]
+    elif mouse_1[1] == bytesAddressPair[1]:
+        mouse_1[0] = bytesAddressPair[0]
+        mouse_1[1] = bytesAddressPair[1]
+    elif mouse_1[1] != bytesAddressPair[1] and mouse_2[1] == 0:
+        mouse_2[0] = bytesAddressPair[0]
+        mouse_2[1] = bytesAddressPair[1]
+    elif mouse_2[1] == bytesAddressPair[1]:
+        mouse_2[0] = bytesAddressPair[0]
+        mouse_2[1] = bytesAddressPair[1]
+    elif mouse_2[1] != bytesAddressPair[1] and mouse_3[1] == 0:
+        mouse_3[0] = bytesAddressPair[0]
+        mouse_3[1] = bytesAddressPair[1]
+    elif mouse_3[1] == bytesAddressPair[1]:
+        mouse_3[0] = bytesAddressPair[0]
+        mouse_3[1] = bytesAddressPair[1]
+    else:
+        print("something went wrong with assigning clients")
 
-    UDPServerSocket.sendto(coord_struct, address)
+
+    #just for testing, remove later
+    clientMsg1 = "Message from Client:{}".format(mouse_1[0])
+    clientIP1  = "Client IP Address:{}".format(mouse_1[1])
+    clientMsg2 = "Message from Client:{}".format(mouse_2[0])
+    clientIP2  = "Client IP Address:{}".format(mouse_2[1])
+    clientMsg3 = "Message from Client:{}".format(mouse_3[0])
+    clientIP3  = "Client IP Address:{}".format(mouse_3[1])
+    print(struct.unpack('ff', mouse_1[0]))
+    #print(clientMsg1)
+    print(clientIP1)
+    #print(struct.unpack('ff', mouse_2[0]))
+    #print(clientIP2)
+    #print(struct.unpack('ff', mouse_3[0]))
+    #print(clientIP3)
+    #end testing section
+    
+    
+    move_req = int(input("enter a move"))
+    coord_struct = struct.pack('ffi', heading_req, v_req, move_req)
+    
+   # Sending a reply to client
+    if mouse_1[1] != 0 :
+        UDPServerSocket.sendto(coord_struct, mouse_1[1])
+    if mouse_2[1] != 0 :
+        UDPServerSocket.sendto(coord_struct, mouse_2[1])
+    if mouse_3[1] != 0 :
+        UDPServerSocket.sendto(coord_struct, mouse_3[1])
 
     
