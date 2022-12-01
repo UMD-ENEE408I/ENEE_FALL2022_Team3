819184564fb483a3aedc00a1f741fa9750d99876
KrstoU <112591449+KrstoU@users.noreply.github.com>
2022-12-01 09:54:37 -0500

    Add files via upload

---
+++ Dancing Robot Project v6/lib/Default Loop Trajectory/TrajectoryTracking.h
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <Adafruit_MCP3008.h>
+#include <Adafruit_MPU6050.h>
+#include <Encoder.h>
+
+// IMU (rotation rate and acceleration)
+Adafruit_MPU6050 mpu;
+
+// Buzzer pin which we will use for indicating IMU initialization failure
+const unsigned int BUZZ = 26;
+const unsigned int BUZZ_CHANNEL = 0;
+
+// Need these pins to turn off light bar ADC chips
+const unsigned int ADC_1_CS = 2;
+const unsigned int ADC_2_CS = 17;
+
+// Battery voltage measurement constants
+const unsigned int VCC_SENSE = 27;
+const float ADC_COUNTS_TO_VOLTS = (2.4 + 1.0) / 1.0 * 3.3 / 4095.0;
+
+// Motor encoder pins
+const unsigned int M1_ENC_A = 39;
+const unsigned int M1_ENC_B = 38;
+const unsigned int M2_ENC_A = 37;
+const unsigned int M2_ENC_B = 36;
+
+// Motor power pins
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+// Motor PWM channels
+const unsigned int M1_IN_1_CHANNEL = 8;
+const unsigned int M1_IN_2_CHANNEL = 9;
+const unsigned int M2_IN_1_CHANNEL = 10;
+const unsigned int M2_IN_2_CHANNEL = 11;
+
+const int M_PWM_FREQ = 5000;
+const int M_PWM_BITS = 8;
+const unsigned int MAX_PWM_VALUE = 255; // Max PWM given 8 bit resolution
+float METERS_PER_TICK = (3.14159 * 0.031) / 360.0;
+float TURNING_RADIUS_METERS = 4.3 / 100.0; // Wheels are about 4.3 cm from pivot point
+
+//function list
+void configure_imu();
+void read_imu(float& w_z);
+void est_imu_bias(float& E_w_z, int N_samples);
+void configure_motor_pins();
+// Positive means forward, negative means backwards
+void set_motors_pwm(float left_pwm, float right_pwm);
+float update_pid(float dt, float kp, float ki, float kd,
+                 float x_d, float x,
+                 float& int_e, float abs_int_e_max, // last_x and int_e are updated by this function
+                 float& last_x);
+void CIRCLE(float t, float a, float& x, float& y, float &axff, float &ayff);
+void BACKCIRCLE(float t, float a, float& x, float& y, float &axff, float &ayff);
+void SPIRAL(float t, float a, float& x, float& y, float &axff, float &ayff);
+void ZIGZAG(float t, float a, float& x, float& y, float &axff, float &ayff);
+void DIAMOND(float t, float a, float& x, float& y, float &axff, float &ayff);
+void STAR(float t, float a, float& x, float& y, float &axff, float &ayff);
+void STOP(float& x, float& y, float &axff, float &ayff, float posx, float posy);
+void LINE(float& x, float& y, float& newx, float& newy, float &axff, float &ayff);
+// Signed angle from (x0, y0) to (x1, y1)
+// assumes norms of these quantities are precomputed
+float signed_angle(float x0, float y0, float n0, float x1, float y1, float n1);
+void setDefault_Trajectory();
+float* defaultLoop(Encoder& enc1, Encoder& enc2, int check, int mode, float posx, float posy, float newx, float newy, float &x, float &y
+, float &BiOm);
+//[0] is v, [1] is w
